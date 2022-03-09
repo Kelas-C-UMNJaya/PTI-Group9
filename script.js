7c6ce44d95909db6c8a9e624b5cb0faeb2e800c5
itshiroto <rivowowor7@gmail.com>
2022-03-09 10:37:47 +0800

    Added basic object for players and statuses

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-console.log("Hello World from DevTools!");
-console.log("Ini dari console")
+class Status {
+  constructor(inAmount, inGrowth, inShrink) {
+    this.amount = inAmount;
+    this.growth = inGrowth;
+    this.shrink = inShrink;
+  }
+
+  // get = { this.amount, growth, shrink };
+  updateAmount = () => {
+    if(!(this.amount >= 1000)) {
+      this.amount += this.growth;
+      this.amount += this.shrink;
+    }
+  }
+  changeGrowth = (val) => { this.growth = val; }
+  changeShrink = (val) => { this.shrink = val; }
+};
+
+const Player = (inName) => {
+  let name = inName;
+  let belajar = new Status(0, 4, 0);
+  let makan = new Status(500, 100, 4);
+  let main = new Status(500, 6, 1);
+  let tidur = new Status(500, 2, 1);
+
+  const getStatus = { name, belajar, makan, main, tidur };
+  const update = () => {
+    belajar.updateAmount();
+    makan.updateAmount();
+    makan.updateAmount();
+    main.updateAmount();
+    console.log([belajar.amount, makan.amount, main.amount, tidur.amount]);
+  }
+  return {
+    getStatus,
+    update,
+  }
+
+};
+
+const DOM = () => {
+
+};
+
+let rivo = Player("Rivo");
+rivo.update();
