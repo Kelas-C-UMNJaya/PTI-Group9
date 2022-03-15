1ce15070b43c5fcb05162a4bb071f08a7b510eb2
itshiroto <rivowowor7@gmail.com>
2022-03-15 20:39:14 +0800

    Linking buttons status into the game controller

--- script.js
+++ script.js
@@ -13,14 +13,20 @@ const DOM = (() => {
     let buttons = document.querySelectorAll(".togglebutton");
     buttons.forEach(button => {
       button.addEventListener("click", (e) => {
+        gameController.toggleActive(e.target.dataset.name);
         button.classList.remove("btn-primary");
         button.classList.add("btn-warning");
         buttons.forEach(button => {
           if (button != e.target) {
             button.classList.remove("btn-warning");
+            button.classList.remove("active");
             button.classList.add("btn-primary");
           }
         });
+        if (!(button.classList.contains("active"))) {
+          button.classList.remove("btn-warning");
+          button.classList.add("btn-primary");
+        }
       });
     })
   })();
@@ -32,7 +38,9 @@ const DOM = (() => {
 })();
 
 class Status {
-  constructor(inAmount, inGrowth, inShrink) {
+  isActive = false;
+  constructor(name, inAmount, inGrowth, inShrink) {
+    this.name = name
     this.amount = inAmount;
     this.growth = inGrowth;
     this.shrink = inShrink;
@@ -49,26 +57,33 @@ class Status {
     this.amount += this.growth;
     if (this.amount === 1000) this.amount = 1000;
   }
+
+  active = () => this.isActive = true;
+  inactive = () => this.isActive = false;
+
   changeGrowth = (val) => { this.growth = val; }
   changeShrink = (val) => { this.shrink = val; }
 };
 
 const Player = (inName) => {
   let name = inName;
-  let belajar = new Status(0, 4, 0);
-  let makan = new Status(500, 100, 4);
-  let main = new Status(500, 6, 1);
-  let tidur = new Status(500, 2, 1);
+  // TODO
+  // let avatar = null;
+  let belajar = new Status("belajar", 0, 4, 0);
+  let makan = new Status("makan", 500, 100, 4);
+  let main = new Status("main", 500, 6, 1);
+  let tidur = new Status("tidur", 500, 2, 1);
 
-  const getStatus = { belajar, makan, main, tidur };
+  const status = { belajar, makan, main, tidur };
   const update = () => {
     belajar.shrinkStats();
     makan.shrinkStats();
     main.shrinkStats();
     tidur.shrinkStats();
   }
+
   return {
-    getStatus,
+    status,
     update,
   }
 };
@@ -85,22 +100,35 @@ const gameController = (() => {
 
   const incClock = () => clock.setMinutes((clock.getMinutes()) + 1)
 
+  const toggleActive = (status) => {
+    player.status[status].active();
+    Object.keys(player.status).forEach((obj) => {
+      if (obj != status) {
+        player.status[obj].inactive()
+      }
+    })
+  }
 
-  const init = (() => {
-    initClock();
-  })();
-
-  setInterval(() => {
+  const gameClock = setInterval(() => {
     player.update()
     incClock();
 
     DOM.updateClock([clock.getHours(), clock.getMinutes()]);
 
-    Object.keys(player.getStatus).forEach(val => {
+    Object.keys(player.status).forEach(val => {
       DOM.updateProgress(
-        val, Math.round(player.getStatus[val].amount / 10)
+        val, Math.round(player.status[val].amount / 10)
       );
     })
   }, 1000);
+
+  const init = (() => {
+    initClock();
+  })();
+
+  return {
+    init,
+    toggleActive,
+  }
 })();
 
