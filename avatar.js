2e68c1ca7d1c9450b372575d3a007988725d3267
Fadhil Dzaky <86292434+FFreezed@users.noreply.github.com>
2022-03-17 22:15:13 +0700

    Mengambil nilai input dari text box nama & gambar

--- avatar.js
+++ avatar.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+function userInput() {
+    let name = document.querySelector("#name-input").value;
+    //get src
+    let image = document.querySelector(".carousel-item.active").querySelector("img").getAttribute("src");
+    let user = {
+        name: name,
+        image: image
+    };
+    return user;
+}
+
+function checkUserInput() {
+    let btn = document.querySelector(".inputbutton");
+    btn.addEventListener("click", () => {
+        console.log(userInput());
+    });
+}
+
+checkUserInput();
